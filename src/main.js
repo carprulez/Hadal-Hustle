582a4e1670317835dac06771cf5eb5cc08d625e0
carprulez <147204410+carprulez@users.noreply.github.com>
2023-10-27 11:10:05 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// Carter Gruebel
+// Hadal Hustle
+// hours spent
+// creative tilt justification
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1080,
+    height: 840,
+    scene: [ Menu, Play ]
+}
+let game = new Phaser.Game(config);
+
+// reserve keyboard var
+let keySPACE;
+
+// set UI size
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
