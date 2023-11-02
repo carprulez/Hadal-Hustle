7d590a2f9f140df49f86b93f949db43deb07ee87
carprulez <147204410+carprulez@users.noreply.github.com>
2023-11-02 15:45:57 -0700

    finished instructions screen, next work on game over screen

--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -4,14 +4,26 @@ class Instructions extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('instructionsScreen', './assets/instructionsHadal.png')
     }
 
     create() {
-        // load how to play diagram here
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '20px',
+            color: '#FFFFFF',
+            align: 'center',
+            fixedWidth: 0
+        }
+        this.add.tileSprite(0, 0, 1080, 840, 'instructionsScreen').setOrigin(0, 0);
+
+        this.add.text(game.config.width/2, game.config.height-60, 'Press SPACE to launch sub', menuConfig).setOrigin(0.5);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
-        // start game after a certain amount of time
+        if(Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.scene.start('playScene');
+        }
     }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,7 +5,7 @@ class Menu extends Phaser.Scene {
 
     preload() {
         this.load.image('titleScreen', './assets/titleScreenHadal.png');
-        this.load.audio('titleMusic', './assets/music.mp3')
+        this.load.audio('titleMusic', './assets/titleMusicHadal.mp3')
     }
 
     create() {
